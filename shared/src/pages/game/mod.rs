e5ed12a35d3a68d6e2104a5f34693745171cd380
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2026-01-09 01:50:35 -0500

    body classing and backgrounds

--- shared/src/pages/game/mod.rs
+++ shared/src/pages/game/mod.rs
@@ -1,22 +1,22 @@
 use leptos::prelude::*;
-use leptos_router::hooks::use_params_map;
-
-use crate::pages::game::session::ProvideSession;
 
 mod session;
 
+stylance::import_style!(pub style, "mod.module.css");
+
 #[component]
 pub fn Game() -> impl IntoView {
-    let params = use_params_map();
-    let game = move || params.with(|params| params.get("game").unwrap_or_default());
-
-    let handle = window_event_listener(leptos::ev::message, |msg| leptos::logging::log!("{msg:?}"));
-    on_cleanup(|| handle.remove());
-
     view! {
-        <ProvideSession game=game()>
-            <p>The game is {game}</p>
-            <iframe src=format!("player?game={}", game()) width=320 height=240 />
-        </ProvideSession>
+        <leptos_meta::Body {..} class=style::game />
+        <main class=style::main>
+            Main
+            <div>
+                Nav
+            </div>
+            <div class=style::content>
+                <div class=style::canvas_iframe>Canvas</div>
+                <div class=style::chat>Chat</div>
+            </div>
+        </main>
     }
 }
--- shared/src/pages/home/mod.rs
+++ shared/src/pages/home/mod.rs
@@ -3,17 +3,17 @@ use leptos::prelude::*;
 mod door;
 mod doors;
 
-stylance::import_style!(pub style, "index.module.css");
+stylance::import_style!(pub style, "mod.module.css");
 
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
+        <leptos_meta::Body {..} class=style::home />
         <leptos_meta::Title text="Yume Nikki Online Project" />
         <leptos_meta::Meta
             name="description"
             content="Play multiplayer Yume Nikki and Yume Nikki fangames for free! Ad-free and no registration required."
         />
-        <div class=style::background />
         <img
             src="https://ynoproject.net/images/logo_yno.png"
             class=style::logo
