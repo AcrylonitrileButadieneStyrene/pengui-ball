e93858f2d5ecef75e639eb045725decfddbce97b
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2026-01-24 21:44:51 -0500

    change dialog to modal and css refactor

--- pages/play/src/modals/mod.rs
+++ pages/play/src/modals/mod.rs
@@ -17,17 +17,36 @@ pub fn Modals() -> impl IntoView {
 #[island]
 pub fn Modal(when: Modals, children: Children) -> impl IntoView {
     let state = crate::state();
+    let node_ref = NodeRef::new();
 
     let on_close = {
         let state = state.clone();
         move |_| state.modal.set(None)
     };
 
+    // if only there was a way to open a dialog modally without code
+    Effect::new(move || {
+        let Some::<leptos::web_sys::HtmlDialogElement>(node) = node_ref.get() else {
+            return;
+        };
+
+        let which = state.modal.get();
+        if which == Some(when) {
+            // the result just indicates if the dialog was opened non-modally
+            // which it never will be unless someone's script messes with it
+            // https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal
+            let _ = node.show_modal();
+        } else {
+            node.close();
+        }
+    });
+
     view! {
         <dialog
+            node_ref=node_ref
             class=style::modal
-            prop:open=move || state.modal.get() == Some(when)
             on:close=on_close
+            autofocus=true // idk if i like this or not
         >
             <form method="dialog">
                 <button>{"\u{2716}"}</button>
@@ -35,4 +54,5 @@ pub fn Modal(when: Modals, children: Children) -> impl IntoView {
             {children()}
         </dialog>
     }
+    .attr("closedby", "any") // leptos is making it as difficult as possible
 }
