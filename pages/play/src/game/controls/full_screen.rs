7b5093dcf57cdd06b48ffd54ca624f521769ec77
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2026-02-10 18:24:41 -0500

    make fullscreen button also leave fullscreen

--- pages/play/src/game/controls/full_screen.rs
+++ pages/play/src/game/controls/full_screen.rs
@@ -3,13 +3,19 @@ use leptos::prelude::*;
 #[island]
 pub fn FullScreen(children: Children) -> impl IntoView {
     let on_click = |_| {
-        // might be better to use a node_ref, but that requires moving the
-        // <main id=layout> element into an island.
-        let Some(layout) = document().get_element_by_id("layout") else {
-            return;
-        };
+        let document = document();
 
-        drop(layout.request_fullscreen());
+        if document.fullscreen_element().is_some() {
+            document.exit_fullscreen();
+        } else {
+            // might be better to use a node_ref, but that requires moving the
+            // <main id=layout> element into an island.
+            let Some(layout) = document.get_element_by_id("layout") else {
+                return;
+            };
+
+            drop(layout.request_fullscreen());
+        }
     };
 
     view! {
