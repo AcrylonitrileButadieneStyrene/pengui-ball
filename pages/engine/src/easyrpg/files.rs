b1aa29aaa660ea6d2482260491353f5316673d31
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2026-02-19 12:33:45 -0500

    save management part 5

--- pages/engine/src/easyrpg/files.rs
+++ pages/engine/src/easyrpg/files.rs
@@ -1,11 +1,22 @@
 use std::sync::Arc;
 
-use leptos::web_sys::js_sys::{Date, Uint8Array};
+use leptos::{
+    prelude::{location, window},
+    web_sys::{
+        js_sys::{Date, Uint8Array},
+        wasm_bindgen::JsValue,
+    },
+};
 
 pub fn get_file(state: &crate::EngineState, id: usize) {
     let game = state.game.clone();
     leptos::task::spawn_local(async move {
-        let (transaction, store, key) = get_store(&game, id).await;
+        let (transaction, store, key, exists) = get_store(&game, id).await;
+        if !exists {
+            transaction.abort().unwrap().await.unwrap();
+            return;
+        }
+
         let value = store
             .get(idb::Query::Key(key.into()))
             .unwrap()
@@ -26,33 +37,60 @@ pub fn set_file(state: &crate::EngineState, id: usize, data: Arc<[u8]>) {
         contents: Uint8Array::new_from_slice(&data),
     };
     leptos::task::spawn_local(async move {
-        let (transaction, store, key) = get_store(&game, id).await;
+        let (transaction, store, key, exists) = get_store(&game, id).await;
+        let key: JsValue = key.into();
+        if exists {
+            // these confirmations are pointless currently but in the future the
+            // engine frame will be cross-origin so then they will be purposeful
+            if !window()
+                .confirm_with_message(&format!(
+                    "An attempt has been made to OVERWRITE the save in slot {id}. Proceed?"
+                ))
+                .unwrap_or_default()
+            {
+                transaction.abort().unwrap().await.unwrap();
+                return;
+            }
+        }
+
         store
-            .put(
-                &serde_wasm_bindgen::to_value(&file).unwrap(),
-                Some(&key.into()),
-            )
+            .put(&serde_wasm_bindgen::to_value(&file).unwrap(), Some(&key))
             .unwrap()
             .await
             .unwrap();
         transaction.commit().unwrap().await.unwrap();
+        location().reload().unwrap();
     });
 }
 
 pub fn delete_file(state: &crate::EngineState, id: usize) {
     let game = state.game.clone();
     leptos::task::spawn_local(async move {
-        let (transaction, store, key) = get_store(&game, id).await;
+        let (transaction, store, key, exists) = get_store(&game, id).await;
+        let confirmed = || {
+            window()
+                .confirm_with_message(&format!(
+                    "An attempt has been made to DELETE the save in slot {id}. Proceed?"
+                ))
+                .unwrap_or_default()
+        };
+
+        if !exists || !confirmed() {
+            transaction.abort().unwrap().await.unwrap();
+            return;
+        }
+
         store
             .delete(idb::Query::Key(key.into()))
             .unwrap()
             .await
             .unwrap();
         transaction.commit().unwrap().await.unwrap();
+        location().reload().unwrap();
     });
 }
 
-async fn get_store(game: &str, id: usize) -> (idb::Transaction, idb::ObjectStore, String) {
+async fn get_store(game: &str, id: usize) -> (idb::Transaction, idb::ObjectStore, String, bool) {
     let factory = idb::Factory::new().unwrap();
     let database = factory
         .open(&format!("/easyrpg/{game}/Save"), None)
@@ -63,12 +101,14 @@ async fn get_store(game: &str, id: usize) -> (idb::Transaction, idb::ObjectStore
         .transaction(&["FILE_DATA"], idb::TransactionMode::ReadWrite)
         .unwrap();
     let store = transaction.object_store("FILE_DATA").unwrap();
-
-    (
-        transaction,
-        store,
-        format!("/easyrpg/{game}/Save/Save{id:>02}.lsd"),
-    )
+    let key = format!("/easyrpg/{game}/Save/Save{id:>02}.lsd");
+    let exists = store
+        .count(Some(idb::Query::Key(JsValue::from_str(&key))))
+        .unwrap()
+        .await
+        .unwrap()
+        != 0;
+    (transaction, store, key, exists)
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
