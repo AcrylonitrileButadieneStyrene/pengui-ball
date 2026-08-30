24d7fe53e7e1af388ee4a49a09df61563414253d
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2026-08-30 13:17:54 -0400

    badges part 6

--- pages/play/src/modals/badge_list.rs
+++ pages/play/src/modals/badge_list.rs
@@ -9,27 +9,50 @@ pub fn Modal() -> impl IntoView {
     view! {
         <super::Modal when=super::Modals::BadgeList>
             <h1>Badges</h1>
-            <WithGameSelector>
+            <Provider>
+                <GameSelector>
+                    <GameTab id="all" label="All" default=true />
+                </GameSelector>
+
                 <div style:height="10000px">
                     <Inner/>
                 </div>
-            </WithGameSelector>
+            </Provider>
         </super::Modal>
     }
 }
 
+#[derive(Clone, Copy)]
+struct SelectedGame(pub RwSignal<Option<Arc<str>>>);
+
 #[island]
-fn WithGameSelector(children: Children) -> impl IntoView {
+fn Provider(children: Children) -> impl IntoView {
+    provide_context(SelectedGame(RwSignal::new(None)));
+
+    children()
+}
+
+#[island]
+fn GameSelector(children: Children) -> impl IntoView {
     let state = crate::state();
+    let selected_game = expect_context::<SelectedGame>();
+
+    let on_change = move |event| {
+        let game = event_target_value(&event);
+        selected_game.0.set(if game == "all" {
+            None
+        } else {
+            Some(game.into())
+        });
+    };
 
     view! {
-        <div class=style::games>
-            <GameTab id="all" label="All" default=true />
+        <div class=style::games on:change=on_change>
+            {children()}
             <For each=state.badges.by_group key=|(game, _)| game.clone() let((game, _))>
                 <GameTab id=game.clone() label=game />
             </For>
         </div>
-        {children()}
     }
 }
 
